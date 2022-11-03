385ee8b5432a521e973aff09d7cb560d8ad3c0dd
Sydney Kuhn <90277234+sydneykuhn@users.noreply.github.com>
2022-11-03 16:07:50 -0400

    Create keymaps.lua

---
+++ .config/lua/user/keymaps.lua
@@ -0,0 +1,12 @@
+local opts = {noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+
+keymap("n", "<leader>e", ":Lex 30<CR>", opts)
