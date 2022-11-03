40c1a53301f67917dca0f7cea739c7c969df4c12
Sydney Kuhn <90277234+sydneykuhn@users.noreply.github.com>
2022-11-03 16:08:49 -0400

    Create options.lua

---
+++ .config/lua/user/options.lua
@@ -0,0 +1,17 @@
+vim.opt.encoding = "utf-8"
+vim.opt.termguicolors = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.numberwidth = 4
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.colorcolumn = {80}
+
+vim.opt.scrolloff = 6
+vim.opt.wrap = false
+
+vim.opt.listchars = { space = '•', eol = '↵' }
+vim.opt.list = true
